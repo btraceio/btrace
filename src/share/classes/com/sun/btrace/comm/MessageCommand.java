57865447b1c60c4392f0210d3d59284afcf05df5
A. Sundararajan <devnull@localhost>
2008-03-06 05:31:29 +0000

    Initial revision Initial revision

---
+++ src/share/classes/com/sun/btrace/comm/MessageCommand.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+package com.sun.btrace.comm;
+
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.IOException;
+
+public class MessageCommand extends Command {
+    private long time;
+    private String msg;
+
+    public MessageCommand(long time, String msg) {
+        super(MESSAGE);
+        this.time = time;
+        this.msg = msg;
+    }
+
+    public MessageCommand(String msg) {
+        this(0L, msg);
+    }
+
+    protected MessageCommand() {
+        this(0L, null);
+    }
+    
+    protected void write(ObjectOutput out) throws IOException {
+        out.writeLong(time);
+        out.writeUTF(msg);
+    }
+
+    protected void read(ObjectInput in) 
+                   throws ClassNotFoundException, IOException {
+        time = in.readLong();
+        msg = in.readUTF();
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getMessage() {
+        return msg;
+    }
+}
