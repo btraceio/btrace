40f208db1f2dbb46af4830a500c3386758d47f8a
A. Sundararajan <devnull@localhost>
2008-03-06 05:34:00 +0000

    Initial revision Initial revision

---
+++ samples/ProbeExit.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+import com.sun.btrace.annotations.*;
+import static com.sun.btrace.BTraceUtils.*;
+
+/**
+ * This program demonstrates OnExit probe.
+ * When some BTrace action method calls "exit(int)"
+ * built-in function, method annotated by @OnExit
+ * (if found) is called. In this method, BTrace script
+ * print summary information of tracing and/or do clean-up.
+ */
+
+@BTrace public class ProbeExit {
+    private static volatile int i;
+
+    // @OnExit is called when some BTrace method
+    // calls exit(int) method
+    @OnExit
+    public static void onexit(int code) {
+        println("BTrace program exits!");
+    }
+
+    // We just put @OnTimer probe and exit BTrace
+    // program when the count reaches 5.
+
+    @OnTimer(1000)
+    public static void ontime() {
+        println("hello");
+        i++;
+        if (i == 5) {
+            // note that this exits the BTrace client
+            // and not the traced program (which would
+            // be a destructive action!).
+            exit(0);
+        }
+    }
+}
---
+++ samples/SocketTracker.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+import com.sun.btrace.annotations.*;
+import static com.sun.btrace.BTraceUtils.*;
+import java.net.*;
+import java.nio.channels.SocketChannel;
+
+/**
+ * This example tracks all server socket creations
+ * and client socket accepts.
+ */
+@BTrace public class SocketTracker {
+    @TLS private static int port = -1;
+    @TLS private static InetAddress inetAddr;
+    @TLS private static SocketAddress sockAddr;
+
+    @OnMethod(
+        clazz="java.net.ServerSocket",
+        method="<init>"
+    )
+    public static void onServerSocket(ServerSocket self, 
+        int p, int backlog, InetAddress bindAddr) {
+        port = p;
+        inetAddr = bindAddr;
+    }
+
+    @OnMethod(
+        clazz="java.net.ServerSocket",
+        method="<init>",
+        type="void (int, int, java.net.InetAddress)",
+        location=@Location(Kind.RETURN)
+    )
+    public static void onSockReturn() {
+        if (port != -1) {
+            println(strcat("server socket at ", str(port)));
+            port = -1;
+        }
+        if (inetAddr != null) {
+            println(strcat("server socket at ", str(inetAddr)));
+            inetAddr = null;
+        }
+    }
+
+    @OnMethod(
+        clazz="java.net.ServerSocket",
+        method="bind"
+    )
+    public static void onBind(ServerSocket self, SocketAddress addr, int backlog) {
+        sockAddr = addr;
+    }
+
+    @OnMethod(
+        clazz="java.net.ServerSocket",
+        method="bind",
+        type="void (java.net.SocketAddress, int)",
+        location=@Location(Kind.RETURN)
+    )
+    public static void onBindReturn() {
+        if (sockAddr != null) {
+            println(strcat("server socket bind ", str(sockAddr)));
+            sockAddr = null;
+        }
+    }
+
+    @OnMethod(
+        clazz="sun.nio.ch.ServerSocketChannelImpl",
+        method="bind"
+    )
+    public static void onBind(Object self, SocketAddress addr, int backlog) {
+        sockAddr = addr;
+    }
+
+    @OnMethod(
+        clazz="sun.nio.ch.ServerSocketChannelImpl",
+        method="bind",
+        type="void (java.net.SocketAddress, int)",
+        location=@Location(Kind.RETURN)
+    )
+    public static void onBindReturn2() {
+        if (sockAddr != null) {
+            println(strcat("server socket bind ", str(sockAddr)));
+            sockAddr = null;
+        }
+    }
+
+    @OnMethod(
+        clazz="java.net.ServerSocket",
+        method="accept",
+        location=@Location(Kind.RETURN)
+    )
+    public static void onAcceptReturn(Socket sock) {
+        if (sock != null) {
+            println(strcat("client socket accept ", str(sock)));
+        }
+    }
+
+    @OnMethod(
+        clazz="sun.nio.ch.ServerSocketChannelImpl",
+        method="socket",
+        location=@Location(Kind.RETURN)
+    )
+    public static void onSocket(ServerSocket ssock) {
+        println(strcat("server socket at ", str(ssock)));
+    }
+
+    @OnMethod(
+        clazz="sun.nio.ch.ServerSocketChannelImpl",
+        method="accept",
+        location=@Location(Kind.RETURN)
+    )
+    public static void onAcceptReturn(SocketChannel sockChan) {
+        if (sockChan != null) {
+            println(strcat("client socket accept ", str(sockChan)));
+        }
+    }
+}
