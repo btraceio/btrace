37b0273e5ef7e8df189179def4cd9b74befd7084
A. Sundararajan <devnull@localhost>
2008-05-02 06:12:45 +0000

    Wrongly committed script with debug flag set to true. Reverting it back. Wrongly committed script with debug flag set to true. Reverting it back.

--- bin/btrace.bat
+++ bin/btrace.bat
@@ -9,7 +9,7 @@ set DEFAULT_BTRACE_HOME=
 if not exist "%BTRACE_HOME%\build\btrace-client.jar" goto noBTraceHome
 
 if "%JAVA_HOME%" == "" goto noJavaHome
-  %JAVA_HOME%/bin/java -Dcom.sun.btrace.probeDescPath=. -Dcom.sun.btrace.dumpClasses=true -Dcom.sun.btrace.debug=false -cp %BTRACE_HOME%/build/btrace-client.jar;%BTRACE_HOME%/build/asm-3.0.jar;%JAVA_HOME%/lib/tools.jar com.sun.btrace.client.Main %*
+  %JAVA_HOME%/bin/java -Dcom.sun.btrace.probeDescPath=. -Dcom.sun.btrace.dumpClasses=false -Dcom.sun.btrace.debug=false -cp %BTRACE_HOME%/build/btrace-client.jar;%BTRACE_HOME%/build/asm-3.0.jar;%JAVA_HOME%/lib/tools.jar com.sun.btrace.client.Main %*
   goto end
 :noJavaHome
   echo Please set JAVA_HOME before running this script
