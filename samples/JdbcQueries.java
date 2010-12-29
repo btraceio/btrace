5c5ad2f0b7faf3b70876f95edb884de746565003
Jaroslav Bachorik <j.bachorik@gmail.com>
2010-12-29 17:01:28 +0100

    updating JdbcQueries sample - using an actual type for @Self causes the handler method not being invoked if the target class has not been loaded before

--- samples/JdbcQueries.java
+++ samples/JdbcQueries.java
@@ -120,7 +120,7 @@ public class JdbcQueries {
     // Otherwise the SQL is in the first argument.
 
     @OnMethod(clazz = "+java.sql.Statement", method = "/execute.*/")
-    public static void onExecute(@Self Statement currentStatement, AnyType[] args) {
+    public static void onExecute(@Self Object currentStatement, AnyType[] args) {
         if (args.length == 0) {
             // No SQL argument; lookup the SQL from the prepared statement
             executingStatement = Collections.get(preparedStatementDescriptions, currentStatement);
