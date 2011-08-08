69bc80e9a0460d388d757bf0cae2a246808b37b5
Jaroslav Bachorik <j.bachorik@gmail.com>
2011-08-08 14:49:12 +0200

    adding sample for location NEWARRAY

---
+++ samples/NewArray.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2008-2010 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+package com.sun.btrace.samples;
+
+import com.sun.btrace.annotations.*;
+import static com.sun.btrace.BTraceUtils.*;
+
+/**
+ * This script demonstrates the possibility to intercept
+ * array creations that are about to be executed from the body of
+ * a certain method. This is achieved by using the {@linkplain Kind#NEWARRAY}
+ * location value.
+ */
+@BTrace public class NewArray {
+    // component count
+    private static volatile long count;
+
+    @OnMethod(
+      clazz="/.*/", // tracking in all classes; can be restricted to specific user classes
+      method="/.*/", // tracking in all methods; can be restricted to specific user methods
+      location=@Location(value=Kind.NEWARRAY, clazz="char")
+    )
+    public static void onnew(@ProbeClassName String pcn, @ProbeMethodName String pmn, String arrType, int dim) {
+        // pcn - allocation place class name
+        // pmn - allocation place method name
+        // **** following two parameters MUST always be in this order
+        // arrType - the actual array type
+        // dim - the array dimension
+
+        // increment counter on new array
+        count++;
+    }
+
+    @OnTimer(2000)
+    public static void print() {
+        // print the counter
+        println(Strings.strcat("char[] count = ", str(count)));
+    }
+}
