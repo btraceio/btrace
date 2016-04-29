fadf494a7949f6a690c17e5fa16d20814972e734
Joachim Haagen Skeie <joachim@haagen-software.no>
2016-04-29 17:37:55 +0200

    Fixing fileRollMaxRolls and fileRollMilliseconds

--- src/share/classes/com/sun/btrace/agent/Main.java
+++ src/share/classes/com/sun/btrace/agent/Main.java
@@ -290,9 +290,25 @@ public final class Main {
                 fileRollMilliseconds = null;
             }
             if (fileRollMilliseconds != null) {
+                debugPrint("fileRollMilliseconds is " + settings.isUnsafe());
+                settings.setFileRollMilliseconds(fileRollMilliseconds.intValue());
                 if (isDebug()) {
                     debugPrint("fileRollMilliseconds is " + fileRollMilliseconds);
                 }
+            }
+        }
+
+        p = argMap.get("fileRollMaxRolls");
+        if (p != null && p.length() > 0) {
+            Integer rolls = null;
+            try {
+                rolls = Integer.parseInt(p);
+            } catch (NumberFormatException nfe) {
+                rolls = null;
+            }
+
+            if (rolls != null) {
+                settings.setFileRollMaxRolls(rolls);
             }
         }
         p = argMap.get("unsafe");
