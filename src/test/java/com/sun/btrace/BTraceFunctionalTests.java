5ccabd02170c53d09ca2cb92783eef9279efbd9d
jbachorik <j.bachorik@gmail.com>
2017-10-22 23:27:05 +0200

    Try update CAs in Travis for gradlew

--- src/test/java/com/sun/btrace/BTraceFunctionalTests.java
+++ src/test/java/com/sun/btrace/BTraceFunctionalTests.java
@@ -125,8 +125,6 @@ public class BTraceFunctionalTests extends RuntimeTest {
 
     @Test
     public void testOnMethod() throws Exception {
-        debugBTrace = true;
-        debugTestApp = true;
         test(
             "resources.Main",
             "traces/OnMethodTest.java",
