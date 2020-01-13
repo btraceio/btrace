c7870633f776907de5fca4c618ffdc2dd21cb77c
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-01-13 11:14:14 +0100

    Do not use ServiceLoader for runtimes

--- btrace-runtime/src/main/java/org/openjdk/btrace/runtime/BTraceRuntimes.java
+++ btrace-runtime/src/main/java/org/openjdk/btrace/runtime/BTraceRuntimes.java
@@ -7,24 +7,38 @@ import org.openjdk.btrace.core.DebugSupport;
 import org.openjdk.btrace.core.comm.CommandListener;
 
 import java.lang.instrument.Instrumentation;
-import java.util.ServiceLoader;
 
 public final class BTraceRuntimes {
-    public static BTraceRuntime.Impl getDefault() {
-        for (BTraceRuntimeImplFactory<?> factory : ServiceLoader.load(BTraceRuntimeImplFactory.class)) {
-            if (factory.isEnabled()) {
-                return factory.getDefault();
+    private static BTraceRuntimeImplFactory<?> FACTORY = null;
+
+    static {
+        loadFactory("org.openjdk.btrace.runtime.BTraceRuntimeImpl_7$Factory");
+        loadFactory("org.openjdk.btrace.runtime.BTraceRuntimeImpl_9$Factory");
+        loadFactory("org.openjdk.btrace.runtime.BTraceRuntimeImpl_11$Factory");
+
+        BTraceRuntimeAccess.registerRuntimeAccessor();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void loadFactory(String clzName) {
+        try {
+            Class<BTraceRuntimeImplFactory<?>> factoryClz = (Class<BTraceRuntimeImplFactory<?>>) ClassLoader.getSystemClassLoader().loadClass(clzName);
+            BTraceRuntimeImplFactory<?> instance = factoryClz.getConstructor().newInstance();
+            if (instance.isEnabled()) {
+                FACTORY = instance;
             }
+        } catch (ClassNotFoundException e) {
+            DebugSupport.warning("Can not load runtime factory: " + clzName);
+        } catch (Exception e) {
+            DebugSupport.warning(e);
         }
-        return null;
+    }
+
+    public static BTraceRuntime.Impl getDefault() {
+        return FACTORY != null ? FACTORY.getDefault() : null;
     }
 
     public static BTraceRuntime.Impl getRuntime(String className, ArgsMap args, CommandListener cmdListener, DebugSupport ds, Instrumentation inst) {
-        for (BTraceRuntimeImplFactory<?> factory : ServiceLoader.load(BTraceRuntimeImplFactory.class)) {
-            if (factory.isEnabled()) {
-                return factory.getRuntime(className, args, cmdListener, ds, inst);
-            }
-        }
-        return null;
+        return FACTORY != null ? FACTORY.getRuntime(className, args, cmdListener, ds, inst) : null;
     }
 }
