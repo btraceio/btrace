a309e4876a3890847e2f84cdee8651c59577a781
Jaroslav Bachorik <j.bachorik@btrace.io>
2024-07-07 23:15:02 +0200

    Refactor trace resources

--- integration-tests/src/test/btrace/OnExitTest.java
+++ integration-tests/src/test/btrace/OnExitTest.java
@@ -22,7 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package traces;
+package btrace;
 
 import org.openjdk.btrace.core.annotations.BTrace;
 import org.openjdk.btrace.core.annotations.OnExit;
--- integration-tests/src/test/btrace/OnMethodLevelTest.java
+++ integration-tests/src/test/btrace/OnMethodLevelTest.java
@@ -20,7 +20,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-package traces;
+package btrace;
 
 import org.openjdk.btrace.core.annotations.*;
 import static org.openjdk.btrace.core.BTraceUtils.*;
