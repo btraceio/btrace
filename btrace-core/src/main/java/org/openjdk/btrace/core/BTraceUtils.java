2b50543c263a1a235e82444fb6659d99347b9b72
teresa <teresa.hong.2023@cis.smu.edu.sg>
2025-05-07 00:26:02 +0800

    Fix CVE-2020-1729
    This change ensures that BTrace properly respects the Java security model when accessing sensitive class loader information, preventing potential security breaches through unauthorized access to the thread context class loader.
    
    References:
    https://github.com/smallrye/smallrye-config/commit/fb0def6f61c09a2a80c9145e4ec6521225cd0b99

--- btrace-core/src/main/java/org/openjdk/btrace/core/BTraceUtils.java
+++ btrace-core/src/main/java/org/openjdk/btrace/core/BTraceUtils.java
@@ -5103,8 +5103,21 @@ public class BTraceUtils {
     }
 
     /** Returns the current context class loader */
-    public static ClassLoader contextClassLoader() {
-      return Thread.currentThread().getContextClassLoader();
+    
+    static ClassLoader getContextClassLoader() {
+      if (System.getSecurityManager() == null) {
+        return Thread.currentThread().getContextClassLoader();
+      } else {
+        return AccessController.doPrivileged((PrivilegedAction<ClassLoader>) () -> {
+          ClassLoader tccl = null;
+          try {
+            tccl = Thread.currentThread().getContextClassLoader();
+          } catch (SecurityException ex) {
+            LOG.warn("Unable to get context classloader instance.", ex);
+          }
+          return tccl;
+        });
+      }
     }
 
     // get Class of the given name
